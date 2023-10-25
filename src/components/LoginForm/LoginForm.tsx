a18e34b81752dae3eced49ab246ba247205d9eba
Bohdan Yunakov <b.yunakov1654@gmail.com>
2023-10-25 15:45:44 +0300

    quick fix

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -12,9 +12,9 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 
 const validationSchema = yup.object({
-  username: yup.string("Enter your username").required("Username is required"),
+  username: yup.string().required("Username is required"),
   password: yup
-    .string("Enter your password")
+    .string()
     .min(6, "Password should be of minimum 6 characters length")
     .required("Password is required"),
 });
